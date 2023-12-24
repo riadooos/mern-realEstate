7c511c31b224a8fc5a719e435a5eb78a2ec293cb
Ryad Touhami <riadt77@hotmail.com>
2023-12-24 22:11:39 +0100

    create header components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
